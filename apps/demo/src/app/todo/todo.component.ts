b8268473f45a320d57053bd825b8d774e197d3f6
devlinduldulao <webmasterdevlin@gmail.com>
2023-06-03 13:29:07 +0200

    update

--- apps/demo/src/app/todo/todo.component.ts
+++ apps/demo/src/app/todo/todo.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '@superheroes/api-interfaces';
+import { TodoService } from './todo.service';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+UntilDestroy()
 @Component({
   selector: 'superheroes-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent {}
+export class TodoComponent implements OnInit {
+  public todos: Todo[] = [];
+  public activeTasks = 0;
+  public newTodo = '';
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+    this.getTodos();
+  }
+
+  getTodos(query = '') {
+    return this.todoService
+      .getTodos(query)
+      .pipe(untilDestroyed(this))
+      .subscribe((todos) => {
+        this.todos = todos;
+        this.activeTasks = this.todos.filter((todo) => !todo.isDone).length;
+      })
+  }
+}
