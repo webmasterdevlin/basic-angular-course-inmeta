430d9ea365133144e9c85e84c71aae8e3812584f
devlinduldulao <webmasterdevlin@gmail.com>
2023-06-03 20:20:03 +0200

    update

--- apps/demo/src/app/todo/todo.service.ts
+++ apps/demo/src/app/todo/todo.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Todo } from '@superheroes/api-interfaces';
+import { HttpClient } from '@angular/common/http';
+import type { Todo } from '@superheroes/api-interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -11,20 +11,25 @@ export class TodoService {
   getTodos(query = '') {
     return this.http.get<Todo[]>(`/api/todo?query=${query}`);
   }
+
   add(data: Todo) {
     return this.http.post<Todo>('/api/todo', data);
   }
+
   put(changed: Todo) {
     return this.http.put<Todo>(`/api/todo/${changed.id}`, changed);
   }
+
   deleteById(selected: Todo) {
-    return this.http.delete<Todo>(`/api/todo/${selected.id}`);
+    return this.http.delete<void>(`/api/todo/${selected.id}`);
   }
+
   deleteCompleted() {
     return this.http.delete<void>(`/api/todo/delete/completed`);
   }
+
   toggle(selected: Todo) {
     selected.isDone = !selected.isDone;
-    return this.http.patch<Todo>(`/api/todo/${selected.id}`, selected);
+    return this.http.put<Todo>(`/api/todo/${selected.id}`, selected);
   }
 }
