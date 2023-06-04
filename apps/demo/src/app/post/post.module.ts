84fac25b3c0717aedf2bd8cd99808a9b9191b1ec
devlinduldulao <webmasterdevlin@gmail.com>
2023-06-04 10:44:09 +0200

    update

---
+++ apps/demo/src/app/post/post.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostComponent } from './apps/demo/src/app/post/post.component';
+
+@NgModule({
+  declarations: [PostComponent],
+  imports: [CommonModule],
+})
+export class PostModule {}
