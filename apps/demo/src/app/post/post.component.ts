8db1a70a929821baeaee4a079b9585ab8f14cf70
devlinduldulao <webmasterdevlin@gmail.com>
2023-06-04 11:18:14 +0200

    update

--- apps/demo/src/app/post/post.component.ts
+++ apps/demo/src/app/post/post.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import type { Post } from '@superheroes/api-interfaces';
+import { Observable } from 'rxjs';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'superheroes-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent {}
+export class PostComponent implements OnInit {
+  posts$: Observable<Post[]>;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.postService.getPosts();
+
+    this.postService.init();
+  }
+
+  getNewPosts() {
+    this.posts$ = this.postService.getNewPosts();
+  }
+}
