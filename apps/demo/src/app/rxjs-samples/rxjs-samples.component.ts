01becebd20128f63a0de6da2a4dd92a522e9a789
devlinduldulao <webmasterdevlin@gmail.com>
2023-06-04 13:24:38 +0200

    update

--- apps/demo/src/app/rxjs-samples/rxjs-samples.component.ts
+++ apps/demo/src/app/rxjs-samples/rxjs-samples.component.ts
@@ -20,7 +20,46 @@ UntilDestroy();
   styleUrls: ['./rxjs-samples.component.css'],
 })
 export class RxjsSamplesComponent implements OnInit {
+  myForm = this.formBuilder.group({
+    querySearchTerm: ['', [Validators.required, Validators.minLength(2)]],
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private sampleService: RxjsSamplesService
+  ) {}
+
   ngOnInit(): void {
-   console.log('')
+    console.log('');
+  }
+
+  mapOperator() {
+    this.sampleService
+      .getPosts()
+      .pipe(
+        // If there is an error, return an empty Observable
+        // this prevents the entire stream from failing
+        catchError((err) => EMPTY),
+        // pipe is like a chain of middleware functions for processing data
+        // chain can have as many functions as you want using different operators
+        map((posts) => {
+          // this log only runs once
+          console.log('log');
+          return posts.map((post) => {
+            // this runs for each post
+            return {
+              id: post.id,
+              headline: post.title,
+              content: post.body,
+            };
+          });
+        }),
+        untilDestroyed(this)
+      )
+      .subscribe((results) => {
+        // end result of the stream
+        console.log(results);
+        console.table(results);
+      });
   }
 }
